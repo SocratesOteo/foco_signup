e7f749682d7d1cba8986427d107a6ac7c7d86fd7
Socrates <socrates@Socratess-iMac.local>
2022-05-04 23:53:38 -0600

    done

---
+++ server/index.js
@@ -0,0 +1,18 @@
+
+const express = require('express')
+const path = require('path')
+const cors = require('cors')
+const port = process.env.PORT || process.env.SERVER_PORT
+
+const app = express()
+app.use (express.json)
+app.use(cors())
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname, "../home.html"))
+})
+
+
+app.listen(port, ()=>{
+    console.log('finally!!!!!!!!')
+})
